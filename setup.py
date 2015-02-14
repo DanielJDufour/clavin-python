520bc7ccdb3cde881efc98759975a22754af554f
Daniel J. Dufour <daniel.j.dufour@gmail.com>
2015-02-14 13:34:50 -0500

    Create setup.py

---
+++ setup.py
@@ -0,0 +1,13 @@
+from distutils.core import setup
+setup(
+  name = 'clavin',
+  packages = ['clavin'],
+  version = '0.1',
+  description = 'A python wrapper for CLAVIN',
+  author = 'Daniel J. Dufour',
+  author_email = 'daniel.j.dufour@gmail.com',
+  url = 'https://github.com/danieljdufour/clavin-python',
+  download_url = 'https://github.com/danieljdufour/clavin-python/tarball/0.1',
+  keywords = ['geotag'],
+  classifiers = [],
+)
