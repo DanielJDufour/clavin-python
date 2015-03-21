b92e20241af10e1aac59425dd386c3572cadae54
DanielJDufour <daniel.j.dufour@gmail.com>
2015-03-21 16:30:13 -0400

    build and download post install

--- clavin/__init__.py
+++ clavin/__init__.py
@@ -1,5 +1,44 @@
 import json, os, re, subprocess
 
+# this is for high-memory machines that have more that 4.5G of memory free
+# this essentially downloads geonames and then builds an index
+def build():
+    ## the code to run after install
+    current_directory = os.path.dirname(os.path.realpath(__file__))
+    if "clavin-java" in os.listdir(current_directory):
+        print "it looks like you have already built the CLAVIN java engine"
+    else:
+        print "building CLAVIN Java engine. this can take a long time"
+        subprocess.call(["curl", "-L", "https://github.com/DanielJDufour/CLAVIN/tarball/3.0", "-o", "clavin-java.tar.gz"], cwd=current_directory)
+        subprocess.call(["tar", "-xvzf", "clavin-java.tar.gz"], cwd=current_directory)
+        subprocess.call(["rm", "clavin-java.tar.gz"], cwd=current_directory)
+        os.rename(d+next(f for f in os.listdir(current_directory) if "DanielJDufour-CLAVIN-" in f),current_directory+"clavin-java")
+        subprocess.call("./script/ci/run_build.sh", cwd=current_directory+"clavin-java")
+
+# download is for low-memory machines
+# it basically downloads the CLAVIN java engine from an Amazon S3
+def download():
+    current_directory = os.path.dirname(os.path.realpath(__file__))
+    print "current directory is", current_directory
+    if "clavin-java" in os.listdir(current_directory):
+        print "it looks like you have already downloaded the CLAVIN java engine"
+    else:
+        urlOfClavinJavaEngine = "https://s3.amazonaws.com/clavinzip/CLAVIN.zip"
+        print "downloading clavin from", urlOfClavinJavaEngine
+        print "this could take 5 to 10 minutes, depending on your machine and internet connection"
+        # try to download with urllib, if that throws an error, use wget
+        try:
+            import urllib
+            urllib.urlretrieve(urlOfClavinJavaEngine,  current_directory + "/CLAVIN.zip")
+        except:
+            print "you don't have urllib installed, so we're going to use wget instead"
+            subprocess.call(["wget", "https://s3.amazonaws.com/clavinzip/CLAVIN.zip"], cwd=current_directory)
+        subprocess.call(["unzip", "CLAVIN.zip"], cwd=current_directory)
+        subprocess.call(["rm", "CLAVIN.zip"], cwd=current_directory)
+        os.rename(current_directory+"/CLAVIN",current_directory+"/clavin-java")
+        print "you have successfully downloaded the pre-build CLAVIN java engine"
+        
+
 def resolve(stringToResolve):
     output = subprocess.Popen("MAVEN_OPTS='-Xmx2g' mvn exec:java -Dexec.mainClass='com.bericotech.clavin.Resolve' -DstringForClavin='" + stringToResolve + "'", cwd=os.path.dirname(os.path.realpath(__file__)) + "/clavin-java", shell=True, stdout=subprocess.PIPE).stdout.read()
     try:
--- setup.py
+++ setup.py
@@ -1,46 +1,14 @@
 #from distutils.core import setup
-from setuptools import setup
-from setuptools.command.develop import develop
-from setuptools.command.install import install
-import os, subprocess
-
-
-def post_install(command_subclass):
-
-    orig_run = command_subclass.run
-
-    def modified_run(self):
-        orig_run(self)
-
-        ## the code to run after install
-        d = os.path.dirname(os.path.realpath(__file__)) + "/clavin/"
-        if "clavin-java" not in os.listdir(d):
-            subprocess.call(["wget", "https://s3.amazonaws.com/clavinzip/CLAVIN.zip"], cwd=d)
-            subprocess.call(["unzip", "CLAVIN.zip"], cwd=d)
-            subprocess.call(["rm", "CLAVIN.zip"], cwd=d)
-            os.rename(d+"/CLAVIN",d+"clavin-java")
-
-    command_subclass.run = modified_run
-    return command_subclass
-
-@post_install
-class CustomDevelopCommand(develop):
-    pass
-
-@post_install
-class CustomInstallCommand(install):
-    pass
 
 setup(
   name = 'clavin',
   packages = ['clavin'],
-  version = '0.6',
-  description = 'A python wrapper for CLAVIN',
+  version = '0.7',
+  description = 'A python wrapper for CLAVIN, a Java engine originally built by Berico Technologies',
   author = 'Daniel J. Dufour',
   author_email = 'daniel.j.dufour@gmail.com',
   url = 'https://github.com/danieljdufour/clavin-python',
-  download_url = 'https://github.com/danieljdufour/clavin-python/tarball/0.6',
+  download_url = 'https://github.com/danieljdufour/clavin-python/tarball/0.7',
   keywords = ['geotag'],
   classifiers = [],
-  cmdclass={'install': CustomInstallCommand},
 )
